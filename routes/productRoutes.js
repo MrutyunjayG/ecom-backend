94680fcde03c4903a3b66f0021612d1be55a1f73
unknown <mrutyunjaygadgimath@gmail.com>
2025-07-26 16:07:01 -1200

    Hllo

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -6,15 +6,19 @@ const {
   updateProduct,
   deleteProduct,
 } = require("../controllers/productcontroller");
+const { isAuth } = require("../middlewares/authMiddlewares");
+const { isAdmin } = require("../middlewares/authMiddlewares");
 
 const productRoutes = express.Router();
 
-productRoutes.get("/products", getAllProducts);
+productRoutes.get("/products", isAuth, getAllProducts);
 
 productRoutes.get("/products/:id", getProductById);
 
-productRoutes.post("/products", createProduct);
+productRoutes.post("/products", isAuth, isAdmin, createProduct);
 
-productRoutes.put("products/:id", updateProduct);
-productRoutes.delete("/products/:id", deleteProduct);
+productRoutes.put("/products/:id", isAuth, isAdmin, updateProduct);
+
+productRoutes.delete("/products/:id", isAuth, isAdmin, deleteProduct);
+
 module.exports = productRoutes;
